6998c573fe5a8cbdaf6fdab9ae1a31c3b28f122a
Daniel I. Martinez <143040076+DIMCSUMB@users.noreply.github.com>
2023-11-16 20:59:20 -0800

    Add files via upload

---
+++ Mystic-Merchants-Domain/app/src/main/java/com/example/mystic_merchants_domain/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.mystic_merchants_domain;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
